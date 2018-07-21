a8f3141f5ba5e64d65c95e6b716ddbb9f0c622db
Vencislav <vencislav.bujic@gmail.com>
2018-07-21 11:53:09 +0300

    added third line

--- a.java
+++ a.java
@@ -2,5 +2,6 @@ public class A {
 	public static void main( String[] args ){
 		System.out.println("Hello, world!");
 		System.out.println("Vencislav here!");
+		System.out.println("Hello from second developer");
 	}
 }
