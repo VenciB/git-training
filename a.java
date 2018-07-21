b81c1a2a099c504f760d051e69dcb937b3298443
Vencislav <vencislav.bujic@gmail.com>
2018-07-21 12:20:57 +0300

    empty main

--- a.java
+++ a.java
@@ -1,7 +1,5 @@
 public class A {
 	public static void main( String[] args ){
-		System.out.println("Hello, world!");
-		System.out.println("Vencislav here!");
-		System.out.println("Hello from second developer");
+
 	}
 }
