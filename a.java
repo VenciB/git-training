215757604a5cc6b57f0612701bad9ee3bbcecf23
Vencislav <vencislav.bujic@gmail.com>
2018-07-21 12:25:19 +0300

    reply to second developer

--- a.java
+++ a.java
@@ -1,6 +1,7 @@
 public class A {
 	public static void main( String[] args ){
 		System.out.println("Hello to first from second developer!");
+		System.out.println("Hello to you too, from first developer!");
 
 	}
 }
